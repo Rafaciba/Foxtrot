c313caf970b08030041a4d43603dbdf34b59fca1
Rafaciba <rafael_f_c_c@hotmail.com>
2017-04-26 21:12:16 -0300

    * Login sem serviço, com shared preferens e singleton; + ProdutosActivity vazia; + SingletonUsuario, que guarda o usuário logado; + Classe Usuario;

---
+++ app/src/main/java/net/sistemasparainter/foxtrot/daragadito/foxtrot/Usuario.java
@@ -0,0 +1,42 @@
+package net.sistemasparainter.foxtrot.daragadito.foxtrot;
+
+/**
+ * Created by rafael.fccibim on 26/04/2017.
+ */
+
+public class Usuario {
+
+    private int idCliente;
+    private String nomeCompletoCliente;
+    private String emailCliente;
+    private String senhaCliente;
+    private String CPFCliente;
+
+    public Usuario(int idCliente, String nomeCompletoCliente, String emailCliente, String senhaCliente, String CPFCliente) {
+        this.idCliente = idCliente;
+        this.nomeCompletoCliente = nomeCompletoCliente;
+        this.emailCliente = emailCliente;
+        this.senhaCliente = senhaCliente;
+        this.CPFCliente = CPFCliente;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public String getNomeCompletoCliente() {
+        return nomeCompletoCliente;
+    }
+
+    public String getEmailCliente() {
+        return emailCliente;
+    }
+
+    public String getSenhaCliente() {
+        return senhaCliente;
+    }
+
+    public String getCPFCliente() {
+        return CPFCliente;
+    }
+}

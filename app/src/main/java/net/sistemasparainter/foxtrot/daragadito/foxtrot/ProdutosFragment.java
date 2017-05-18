d952a4620e76712ed3f28db6d405f3d02ac8040d
Danilo Garcia <daniloba2000@yahoo.com.br>
2017-05-18 19:23:04 -0300

    Criação da View Produtos utilizando Linear e Relative Layout

--- app/src/main/java/net/sistemasparainter/foxtrot/daragadito/foxtrot/ProdutosFragment.java
+++ app/src/main/java/net/sistemasparainter/foxtrot/daragadito/foxtrot/ProdutosFragment.java
@@ -3,15 +3,23 @@ package net.sistemasparainter.foxtrot.daragadito.foxtrot;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProdutosFragment extends Fragment {
+
 
     private ViewGroup linearContainer;
 
@@ -23,8 +31,28 @@ public class ProdutosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ArrayList<String> urls = new ArrayList<>();
+        urls.add("https://meuorixa.files.wordpress.com/2012/06/quartinhas_blog.jpg");
+        urls.add("https://riodegraca.files.wordpress.com/2016/07/casa_suica.jpg");
+        urls.add("https://i2.wp.com/oprofessorweb.files.wordpress.com/2017/04/figura-4.jpg");
+
+        for (int i = 0; i < 3; i++) {
+
+            addCardView(urls.get(i));
+        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_produtos, container, false);
     }
+    private void addCardView(String url) {
+        CardView cardView = (CardView) LayoutInflater.from(this).inflate(R.layout.fragment_produtos_cardview, linearContainer, false);
+        ImageView verImagem = (ImageView) cardView.findViewById(R.id.imageView);
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+        imageLoader.displayImage(url, verImagem);
 
+        linearContainer.addView(cardView);
+
+    }
 }

d58436dc792848603c65c71973ffe2a6d646bca3
6abriiel1 <gabriel_lopes.g@hotmail.com>
2017-05-18 22:07:30 -0300

    Signeton Carrinho

--- app/src/main/java/net/sistemasparainter/foxtrot/daragadito/foxtrot/Produto.java
+++ app/src/main/java/net/sistemasparainter/foxtrot/daragadito/foxtrot/Produto.java
@@ -28,4 +28,84 @@ public class Produto {
         this.qtdMinEstoque = qtdMinEstoque;
         this.imagem = imagem;
     }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getDescProduto() {
+        return descProduto;
+    }
+
+    public void setDescProduto(String descProduto) {
+        this.descProduto = descProduto;
+    }
+
+    public BigDecimal getPrecProduto() {
+        return precProduto;
+    }
+
+    public void setPrecProduto(BigDecimal precProduto) {
+        this.precProduto = precProduto;
+    }
+
+    public BigDecimal getDescontoPromocao() {
+        return descontoPromocao;
+    }
+
+    public void setDescontoPromocao(BigDecimal descontoPromocao) {
+        this.descontoPromocao = descontoPromocao;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public boolean isAtivoProduto() {
+        return ativoProduto;
+    }
+
+    public void setAtivoProduto(boolean ativoProduto) {
+        this.ativoProduto = ativoProduto;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getQtdMinEstoque() {
+        return qtdMinEstoque;
+    }
+
+    public void setQtdMinEstoque(int qtdMinEstoque) {
+        this.qtdMinEstoque = qtdMinEstoque;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
 }
